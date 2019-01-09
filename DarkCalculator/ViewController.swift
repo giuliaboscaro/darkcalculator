e83b8ec1fd338f6cf39cbd899af5c395d74645e0
giuliaboscaro <giulia.boscaro@hotmail.com>
2019-01-09 15:54:32 -0200

    Add functionality to show the numbers pressed on the display

--- DarkCalculator/ViewController.swift
+++ DarkCalculator/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var finishedTypingNumber: Bool = true
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        finishedTypingNumber = true
+        
+        
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numberValue = sender.currentTitle {
+            if finishedTypingNumber {
+                displayLabel.text = numberValue
+                finishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numberValue
+            }
+        }
+    }
+    
+    func resetLabel() {
+        displayLabel.text = "0"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resetLabel()
     }
 
 
